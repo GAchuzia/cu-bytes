d3fc3547326c4864bb7f0dcd1304e382719736e0
Jake Siushansian <jakesiushansian@cmail.carleton.ca>
2026-01-08 13:04:16 -0500

    fix: added comments to the context file

--- frontend/cu-bytes/app/context.tsx
+++ frontend/cu-bytes/app/context.tsx
@@ -1,12 +1,18 @@
 import { createContext, useContext, useState, ReactNode } from 'react';
 
+// The variable for storing the username of the logged-in user
+// and the setter for setting the value of the username
 type UserContextType = {
     user: string;
     setUser: (user: string) => void;
 }
 
+// Create the state that will be used to store the username of the logged-in user
+// and share this state across the different components (pages represented by the tsx files)
 const UserContext = createContext<UserContextType | undefined>(undefined);
 
+// Set up a component that can accept child components (tsx files)
+// and pass on a variable and its setter to these child components
 export const UserProvider = ({ children }: { children: ReactNode }) => {
     const [user, setUser] = useState('');
 
@@ -17,6 +23,7 @@ export const UserProvider = ({ children }: { children: ReactNode }) => {
     );
 };
 
+// The component that the child components (tsx files) use to access the state
 export const useUser = () => {
     const context = useContext(UserContext);
 
