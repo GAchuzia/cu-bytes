675ef87ffa102d61954fb5111f9d8fc8060c1935
Jake Siushansian <jakesiushansian@cmail.carleton.ca>
2026-01-15 14:19:56 -0500

    feat: Can now view all food items at a dining location and select food items to view their properties. Updated get all food items endpoint to return the food item's dining location id

--- frontend/cu-bytes/app/dining.tsx
+++ frontend/cu-bytes/app/dining.tsx
@@ -11,7 +11,8 @@ import { styles } from "./styles/style-dining";
 export default function DiningScreen() {
 
     const [loading, setLoading] = useState(true);
-    const [visible, setVisible] = useState(false);
+    const [diningLocationsVisible, setDiningLocationsVisible] = useState(true);
+    const [foodItemsVisible, setFoodItemsVisible] = useState(true);
 
     // Get the variables and setters used to access and modify a copy of the user profile elements
     const
@@ -126,6 +127,7 @@ export default function DiningScreen() {
             .then(data => {
                 setFoodItemArray(data);
                 setLoading(false);
+                console.log(data);
             })
         };
         handlePressGetFoodItems();
@@ -155,8 +157,8 @@ export default function DiningScreen() {
     // Filter the array of food items (retrieved from the backend database)
     //
     //
-    const filterFoodItemArray = () => {
-        const filteredFoodItemArray = foodItemArray.filter(foodItem => (foodItem["name"].toLowerCase() as string).includes(foodItemName.toLowerCase()));
+    const filterFoodItemArray = (diningLocationId: number) => {
+        const filteredFoodItemArray = foodItemArray.filter(foodItem => ((foodItem["location"] as number) == diningLocationId));
         setFilteredFoodItemArray(filteredFoodItemArray);
     }
 
@@ -164,11 +166,11 @@ export default function DiningScreen() {
     const [diningLocation, setDiningLocation] = useState(
         {
             "id": 1, // initial value of 1 to prevent errors
-            "dining_location": ""
+            "name": ""
         }
     )
     const [diningLocationId, setDiningLocationId] = useState(diningLocation["id"]);
-    const [diningLocationName, setDiningLocationName] = useState(diningLocation["dining_location"]);
+    const [diningLocationName, setDiningLocationName] = useState(diningLocation["name"]);
 
     // Variables and setters for storing dining location JSON objects
     const [diningLocationArray, setDiningLocationArray] = useState([]);
@@ -184,46 +186,35 @@ export default function DiningScreen() {
 
    // Sends a get all dining locations request to the server exactly once
    useEffect(() => {
-    const handlePressGetDiningLocations = () => {
-        fetch("http://127.0.0.1:5000//locations/dining-locations", {
-                method: "GET"
-            }
-        )
-        .then(response => {
-            if (!response.ok) {
-                throw new Error (`HTTP error! status: ${response.status}`);
-            }
-            return response.json();
-        })
-        .then(data => {
-            // setDiningLocationArray(data);
-            setLoading(false);
-        })
-    };
-    handlePressGetDiningLocations();
+        const handlePressGetDiningLocations = () => {
+            fetch("http://127.0.0.1:5000//locations/dining-locations", {
+                    method: "GET"
+                }
+            )
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error (`HTTP error! status: ${response.status}`);
+                }
+                return response.json();
+            })
+            .then(data => {
+                setDiningLocationArray(data);
+                setLoading(false);
+                console.log(data);
+            })
+        };
+        handlePressGetDiningLocations();
    }, []);
 
-   // Sends a get dining location by id request to the server
-   function handlePressGetDiningLocation(diningLocationId: number) {
-        const diningLocationRequest = `http://127.0.0.1:5000//locations/dining-locations/${diningLocationId}`;
-
-        fetch(diningLocationRequest, {
-                method: "GET"
-            }
-        )
-        .then(response => {
-            if (!response.ok) {
-                throw new Error (`HTTP error! status: ${response.status}`);
-            }
-            return response.json();
-        })
-        .then(data => {
-            setDiningLocation(data);
-            setDiningLocationId(diningLocation["id"]);
-            console.log(data);
-        })
-    
-   }
+   /*
+   Filter the array of dining locations (retrieved from the backend database)
+   The dining location name, entered in the text input, is used to filter the array
+   The dining location whose names partially or wholly match the entered value are stored in a filtered array
+   */
+    const filterDiningLocationArray = () => {
+        const filteredDiningLocationArray = diningLocationArray.filter(diningLocation => (diningLocation["name"].toLowerCase() as string).includes(diningLocationName.toLowerCase()));
+        setFilteredDiningLocationArray(filteredDiningLocationArray);
+    }
 
     // Display loading symbol while the dining locations and food items are being fetched
     if (loading) {
@@ -246,7 +237,200 @@ export default function DiningScreen() {
 
             <Text style={styles.subtitle}>Enter the dining location manually</Text>
 
-            {/* */}
+            {/* Enter the dining location name to list dining locations with similar or matching names */}
+            <TextInput
+                style={styles.textInput}
+                onChange={saveDiningLocationName}
+                placeholder={"Enter the dining location name"}
+                value={diningLocationName}
+            >
+            </TextInput>
+
+            {/* Filter the dining locations in the frontend array by the dining location name and store in another array */}
+            <TouchableOpacity
+                style={[styles.button, loading && styles.buttonDisabled]}
+                onPress={() => {
+                    setDiningLocationsVisible(true);
+                    setFoodItemsVisible(false);
+
+                    filterDiningLocationArray();
+                    console.log(diningLocationsVisible);
+                }}
+                disabled={loading}
+            >
+                <Text style={styles.buttonText}>Confirm</Text>
+            </TouchableOpacity>
+
+            {/* If the entered string value does not return any dining locations, display the following message */}
+            {filteredDiningLocationArray.length == 0 && diningLocationsVisible && (
+                <View>
+                    <Text style={styles.pressableText}>
+                        There are no dining locations on campus that match this search
+                    </Text>
+                </View>
+            )}
+
+            {/* If the entered string value returns any dining locations, display the name and id of each dining location */}
+            {filteredDiningLocationArray && diningLocationsVisible && (
+                <View>
+                    {filteredDiningLocationArray.map((diningLocation) => (
+                        <Text
+                            style={styles.pressableText}
+                            key={diningLocation["id"]}
+                            onPress={() => {
+                                filterFoodItemArray(diningLocation["id"]);
+
+                                setDiningLocationsVisible(false);
+                                setFoodItemsVisible(true);
+                            }}
+                        >
+                            {diningLocation["name"]}{"\n"}(ID {diningLocation["id"]})
+                            <br></br>
+                            <line>---</line>
+                        </Text>
+                    ))}
+                </View>
+            )}
+
+            {filteredFoodItemArray && foodItemsVisible && (
+                <View>
+                    {filteredFoodItemArray.map((foodItem) => (
+                        <Text 
+                            style={styles.pressableText}
+                            key={foodItem["id"]}
+                            onPress={() => {
+                                handlePressGetFoodItem(foodItem["id"]);
+                                setFoodItemsVisible(false);
+                            }}
+                        >
+                            {foodItem["name"]}{"\n"}(ID {foodItem["id"]})
+                            <br></br>
+                            <line>---</line>
+                        </Text>
+                    ))}
+                </View>
+            )}
+
+            {foodItem && (
+
+                <View>
+                    <Text style={styles.subsubtitle}>{foodItem.name}</Text>
+                    <br></br>
+                    <Text style={styles.subsubtitle}>Calories: {processFoodItemCalories(foodItem.calories)}</Text>
+                    <br></br>
+                    <Text style={styles.subsubtitle}>Dining Location: {foodItem.dining_location}</Text>
+                    <br></br>
+                    <Text style={styles.subsubtitle}>Cost: $ {processFoodItemCost(foodItem.cost)}</Text>
+                    <br></br>
+
+                    {/* Display warnings to the logged-in user based on the food item properties and the user's own profile settings */}
+                    
+                    {foodItem.has_eggs == true && (
+                        <Text style={styles.subsubtitle}>{foodItem.has_eggs === true && hasEggAllergyGlobal ? "Warning - this item contains eggs" : null}</Text>
+                    )}
+                    {foodItem.has_eggs == null && (
+                        <Text style={styles.subsubtitle}>{foodItem.has_eggs === null && hasEggAllergyGlobal ? "Warning - this item may contain eggs" : null}</Text>
+                    )}
+
+                    {foodItem.has_fish == true && (
+                        <Text style={styles.subsubtitle}>{foodItem.has_fish === true && hasFishAllergyGlobal ? "Warning - this item contains fish" : null}</Text>
+                    )}
+                    {foodItem.has_fish == null && (
+                        <Text style={styles.subsubtitle}>{foodItem.has_fish === null && hasFishAllergyGlobal ? "Warning - this item may contain fish" : null}</Text>
+                    )}
+
+                    {foodItem.is_dairy_free == true && (
+                        <Text style={styles.subsubtitle}>{foodItem.is_dairy_free === false && hasDairyIntoleranceGlobal ? "Warning - this item contains dairy" : null}</Text>
+                    )}
+                     {foodItem.is_dairy_free == null && (
+                        <Text style={styles.subsubtitle}>{foodItem.is_dairy_free === null && hasDairyIntoleranceGlobal ? "Warning - this item may contain dairy" : null}</Text>
+                    )}
+
+                    {foodItem.has_milk == true && (
+                        <Text style={styles.subsubtitle}>{foodItem.has_milk === true && hasMilkAllergyGlobal ? "Warning - this item contains milk" : null}</Text>
+                    )}
+                    {foodItem.has_milk == null && (
+                        <Text style={styles.subsubtitle}>{foodItem.has_milk === null && hasMilkAllergyGlobal ? "Warning - this item may contain milk" : null}</Text>
+                    )}
+
+                    {foodItem.has_peanuts == true && (
+                        <Text style={styles.subsubtitle}>{foodItem.has_peanuts === true && hasPeanutAllergyGlobal ? "Warning - this item contains peanuts" : null}</Text>
+                    )}
+                    {foodItem.has_peanuts == null && (
+                        <Text style={styles.subsubtitle}>{foodItem.has_peanuts === null && hasPeanutAllergyGlobal ? "Warning - this item may contain peanuts" : null}</Text>
+                    )}
+
+                    {foodItem.has_sesame == true && (
+                        <Text style={styles.subsubtitle}>{foodItem.has_sesame === true && hasSesameAllergyGlobal ? "Warning - this item contains sesame" : null}</Text>
+                    )}
+                    {foodItem.has_sesame == null && (
+                        <Text style={styles.subsubtitle}>{foodItem.has_sesame === null && hasSesameAllergyGlobal ? "Warning - this item may contain sesame" : null}</Text>
+                    )}
+
+                    {foodItem.has_shellfish == true && (
+                        <Text style={styles.subsubtitle}>{foodItem.has_shellfish === true && hasShellfishAllergyGlobal ? "Warning - this item contains shellfish" : null}</Text>
+                    )}    
+                    {foodItem.has_shellfish == null && (
+                        <Text style={styles.subsubtitle}>{foodItem.has_shellfish === null && hasShellfishAllergyGlobal ? "Warning - this item may contain shellfish" : null}</Text>
+                    )}
+
+                    {foodItem.has_soy == true && (
+                        <Text style={styles.subsubtitle}>{foodItem.has_soy === true && hasSoyAllergyGlobal ? "Warning - this item contains soy" : null}</Text>
+                    )}
+                    {foodItem.has_soy == null && (                    
+                        <Text style={styles.subsubtitle}>{foodItem.has_soy === null && hasSoyAllergyGlobal ? "Warning - this item may contain soy" : null}</Text>
+                    )}
+                    
+                    {foodItem.has_treenuts == true && (
+                        <Text style={styles.subsubtitle}>{foodItem.has_treenuts === true && hasTreenutAllergyGlobal ? "Warning - this item contains treenuts" : null}</Text>
+                    )}                    
+                    {foodItem.has_treenuts == null && (
+                        <Text style={styles.subsubtitle}>{foodItem.has_treenuts === null && hasTreenutAllergyGlobal ? "Warning - this item may contain treenuts" : null}</Text>
+                    )}
+
+                    {foodItem.has_wheat == true && (                    
+                        <Text style={styles.subsubtitle}>{foodItem.has_wheat === true && hasWheatAllergyGlobal ? "Warning - this item contains wheat" : null}</Text>
+                    )}
+                    {foodItem.has_wheat == null && (
+                        <Text style={styles.subsubtitle}>{foodItem.has_wheat === null && hasWheatAllergyGlobal ? "Warning - this item may contain wheat" : null}</Text>
+                    )}
+
+                    {foodItem.is_gluten_free == false && (
+                        <Text style={styles.subsubtitle}>{foodItem.is_gluten_free === false && hasGlutenAllergyGlobal ? "Warning - this item contains gluten" : null}</Text>
+                    )}
+                    {foodItem.is_gluten_free == null && (
+                        <Text style={styles.subsubtitle}>{foodItem.is_gluten_free === null && hasGlutenAllergyGlobal ? "Warning - this item may contain gluten" : null}</Text>
+                    )}
+
+                    {foodItem.is_vegan == false && (
+                        <Text style={styles.subsubtitle}>{foodItem.is_vegan === false && isVeganGlobal ?  "Warning - this item is not vegan" : null}</Text>
+                    )}
+                    {foodItem.is_vegan == null && (
+                        <Text style={styles.subsubtitle}>{foodItem.is_vegan === null && isVeganGlobal ?  "Warning - this item may not be vegan" : null}</Text>
+                    )}
+
+                    {foodItem.is_vegetarian == false && (
+                        <Text style={styles.subsubtitle}>{foodItem.is_vegetarian === false && isVegetarianGlobal ?  "Warning - this item is not vegetarian" : null}</Text>
+                    )}
+                    {foodItem.is_vegetarian == null && (
+                        <Text style={styles.subsubtitle}>{foodItem.is_vegetarian === null && isVegetarianGlobal ?  "Warning - this item may not be vegetarian" : null}</Text>
+                    )}
+
+                    {foodItem.is_kosher == false && (
+                        <Text style={styles.subsubtitle}>{foodItem.is_kosher === false && prefersKosherGlobal ? "Warning - this item is not kosher" : null}</Text>
+                    )}
+                    {foodItem.is_kosher == null && (
+                        <Text style={styles.subsubtitle}>{foodItem.is_kosher === null && prefersKosherGlobal ? "Warning - this item may not be kosher" : null}</Text>
+                    )}    
+                        
+                    {foodItem.is_halal == false && (                        
+                        <Text style={styles.subsubtitle}>{foodItem.is_halal === false && prefersHalalGlobal ? "Warning - this item is not halal" : null}</Text>
+                    )}
+                    {foodItem.is_halal == null && (
+                        <Text style={styles.subsubtitle}>{foodItem.is_halal === null && prefersHalalGlobal ? "Warning - this item may not be halal" : null}</Text>
+                    )}
+                </View>
+            )}
 
         </View>
     )
