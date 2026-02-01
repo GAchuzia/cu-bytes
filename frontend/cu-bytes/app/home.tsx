580c0aba1420b3fada7671553b06845f06348b5d
Jake Siushansian <jakesiushansian@cmail.carleton.ca>
2026-01-31 20:24:19 -0500

    chore: updated the gui of the frontend home page and updated the associated stylesheet

--- frontend/cu-bytes/app/home.tsx
+++ frontend/cu-bytes/app/home.tsx
@@ -10,6 +10,15 @@ import { useUser } from './context';
 export default function HomeScreen() {
 
     const [loading, setLoading] = useState(false);
+    const [isButtonPressed, setIsButtonPressed] = useState(false);
+
+    const [isScanFoodItemPressed, setIsScanFoodItemPressed] = useState(false);
+    const [isBrowseFoodItemsPressed, setIsBrowseFoodItemsPressed] = useState(false);
+    const [isBrowseDiningLocationsPressed, setIsBrowseDiningLocationsPressed] = useState(false);
+    const [isViewSavedFoodItemsPressed, setIsViewSavedFoodItemsPressed] = useState(false);
+    const [isGoalsPressed, setIsGoalsPressed] = useState(false);
+    const [isSettingsPressed, setIsSettingsPressed] = useState(false);
+
     /*
         Variables used to store a copy of the logged-in user's username and profile settings 
     */
@@ -22,63 +31,137 @@ export default function HomeScreen() {
     return (
 
         <View style={styles.container}>
-            <StatusBar style="auto" />
+            <StatusBar
+                style="auto"
+                hidden={true}
+            />
 
-            <Text style={styles.subtitle}>{usernameGlobal != "" ? `Logged in as ${usernameGlobal}` : "Not logged in"}</Text>
+            <View
+                style={styles.statusbar}>
 
-            <Text style={styles.title}>Home</Text>
+                <View style={styles.headerContainer}></View>
+                
+                <View style={styles.headerContainer}></View>
 
-            {/* Route the user to the 'scan and identify a food item' page */}
-            <TouchableOpacity
-                style={[styles.button, loading && styles.buttonDisabled]}
+                <Text id="homeTitle"
+                    style={styles.headerTitle}>
+                
+                    Home
+                </Text>
+
+                <View style={styles.headerContainer}></View>
+
+                <Text id="loggedInUser"
+                    style={styles.headerUsernameIcon}>
+
+                    {usernameGlobal != '' ? `${usernameGlobal}` : 'Guest' }
+                </Text>
+
+            </View>
+
+            <Text id="homeInfo"
+                style={styles.infoText}>
+
+                What would you like to do?
+            </Text>
+
+            {/* Route the user to the 'scan food item' page */}
+            <TouchableOpacity id="scanFoodItemButton"
+                style={[styles.bodyButton,
+                    { backgroundColor: isScanFoodItemPressed || usernameGlobal == '' ? '#666666' : '#131312' }
+                ]}
+                onPressIn={() => setIsScanFoodItemPressed(true)}
+                onPressOut={() => setIsScanFoodItemPressed(false)}
                 onPress={() => router.push("/scan")}
-                disabled={loading}
             >
-                <Text style={styles.buttonText}>
-                    Scan and Identify a Food Item
+
+                <Text id="scanFoodItemButtonText"
+                    style={styles.bodyButtonText}>
+                    
+                    Scan Food Item
                 </Text>
             </TouchableOpacity>
 
-            {/* Route the user to the 'browse food items by dining location' page */}
-            <TouchableOpacity
-                style={[styles.button, loading && styles.buttonDisabled]}
+            {/* Route the user to the 'browse food items' page */}
+            <TouchableOpacity id="browseFoodItemsButton"
+                style={[styles.bodyButton,
+                    { backgroundColor: isBrowseFoodItemsPressed || usernameGlobal == '' ? '#666666' : '#131312' }
+                ]}
+                onPressIn={() => setIsBrowseFoodItemsPressed(true)}
+                onPressOut={() => setIsBrowseFoodItemsPressed(false)}
+                onPress={() => router.push("/enter")}
+            >
+                <Text id="browseFoodItemsButtonText"
+                    style={styles.bodyButtonText}>
+                    
+                    Browse Food Items
+                </Text>
+            </TouchableOpacity>
+
+            {/* Route the user to the 'browse dining locations' page */}
+            <TouchableOpacity id="browseDiningLocationsButton"
+                style={[styles.bodyButton,
+                    { backgroundColor: isBrowseDiningLocationsPressed || usernameGlobal == '' ? '#666666' : '#131312' }
+                ]}
+                onPressIn={() => setIsBrowseDiningLocationsPressed(true)}
+                onPressOut={() => setIsBrowseDiningLocationsPressed(false)}
                 onPress={() => router.push("/dining")}
-                disabled={loading}
             >
-                <Text style={styles.buttonText}>
-                    Browse Food Items by Dining Location
+                <Text id="browseDiningLocationsButtonText"
+                    style={styles.bodyButtonText}>
+                    
+                    Browse Dining Locations
                 </Text>
             </TouchableOpacity>
 
-            {/* Route the user to the 'view my saved food items' page */}
-            <TouchableOpacity
-                style={[styles.button, loading && styles.buttonDisabled]}
+            {/* Route the user to the 'view saved food items' page */}
+            <TouchableOpacity id="viewSavedFoodItemsButton"
+                style={[styles.bodyButton,
+                    { backgroundColor: isViewSavedFoodItemsPressed || usernameGlobal == '' ? '#666666' : '#131312' }
+                ]}
+
+                onPressIn={() => setIsViewSavedFoodItemsPressed(true)}
+                onPressOut={() => setIsViewSavedFoodItemsPressed(false)}
                 onPress={() => router.push("/entries")}
-                disabled={loading}
+                disabled={ usernameGlobal == '' ? true : false }
             >
-                <Text style={styles.buttonText}>
-                    View My Saved Food Items
+                <Text id="viewSavedFoodItemsButtonText"
+                    style={styles.bodyButtonText}>
+                    
+                    View Saved Food Items
                 </Text>
             </TouchableOpacity>
 
             {/* Route the user to the 'goals' page */}
-            <TouchableOpacity
-                style={[styles.button, loading && styles.buttonDisabled]}
-                //onPress={() => router.push("/goals")}
-                disabled={loading}
+            <TouchableOpacity id="goalsButton"
+                style={[styles.bodyButton,
+                    { backgroundColor: isGoalsPressed || usernameGlobal == '' ? '#666666' : '#131312' }
+                ]}
+                onPressIn={() => setIsGoalsPressed(true)}
+                onPressOut={() => setIsGoalsPressed(false)}
+                onPress={() => router.push("/goals")}
+                disabled={ usernameGlobal == '' ? true : false }
             >
-                <Text style={styles.buttonText}>
+                <Text id="goalsButtonText" 
+                    style={styles.bodyButtonText}>
+                    
                     Goals
                 </Text>
             </TouchableOpacity>
 
             {/* Route the user to the 'settings' page */}
-            <TouchableOpacity
-                style={[styles.button, loading && styles.buttonDisabled]}
+            <TouchableOpacity id="settingsButton"
+                style={[styles.bodyButton,
+                    { backgroundColor: isSettingsPressed || usernameGlobal == '' ? '#666666' : '#131312' }
+                ]}
+                onPressIn={() => setIsSettingsPressed(true)}
+                onPressOut={() => setIsSettingsPressed(false)}
                 onPress={() => router.push("/settings")}
-                disabled={loading}
+                disabled={ usernameGlobal == '' ? true : false }
             >
-                <Text style={styles.buttonText}>
+                <Text id="settingsButtonText" 
+                    style={styles.bodyButtonText}>
+                    
                     Settings
                 </Text>
             </TouchableOpacity>
--- frontend/cu-bytes/app/styles/style-home.tsx
+++ frontend/cu-bytes/app/styles/style-home.tsx
@@ -3,77 +3,106 @@ import { StyleSheet } from 'react-native';
 export const styles = StyleSheet.create({
 
   container: {
+    alignItems: 'center', // Horizontal
+    backgroundColor: '#C5151A',
     flex: 1,
-
-    backgroundColor: '#FFFFFF',
-    padding: '0.5%' as any,
-
-    alignItems: 'center',
+    flexDirection: 'column',
+    justifyContent: 'flex-start', // Vertical 
+    overflowY: 'scroll',
     textAlign: 'center',
-
-    width: '100%',
-
-    overflowY: 'scroll'
+    width: '100%' as any,
   },
 
-  title: {
-    color: '#0a7ea4',
-    backgroundColor: '#FFFFFFFF',
+  headerContainer: {
+    width: '10%' as any,
+  },
+
+  statusbar: {
+    alignItems: 'center', // Horizontal
+    backgroundColor: '#C5151A',
+    flexDirection: 'row',
+    justifyContent: 'space-between', // Vertical
     padding: '1.5%' as any,
-
     textAlign: 'center',
-    margin: '0.5%' as any,  
-    
-    fontFamily: 'sans-serif',
-    fontSize: '300%' as any,
-    fontWeight: 'bold',   
+    width: '100%' as any,
   },
 
-  subtitle: {
-    color: '#0a7ea4',
-    backgroundColor: '#FFFFFF',
-    padding: '0.5%' as any,
-
-    textAlign: 'center',
-    margin: '0.5%' as any,
-
-    fontFamily: 'sans-serif',
-    fontSize: '175%' as any,
-    fontWeight: 'bold'
-  },
-
-  button: {
-    color: 'black',
-    backgroundColor: '#0a7ea4',
-    padding: '0.5%' as any,
-
-    justifyContent: 'center',
+  headerButton: {
     alignItems: 'center',
-    margin: '1.2%' as any,
-
-    borderRadius: 25,
+    backgroundColor: '#131312',
+    borderRadius: 50,
     borderWidth: 4,
-    borderBlockColor: 'black',
-    
-    shadowColor: 'black',
-    shadowOffset: { width: 1, height: 1 },
-    shadowOpacity: 0.2,
-    shadowRadius: 8,
-    
+    borderColor: 'black',
+    color: 'red',
+    padding: '1.5%' as any,
+    width: '10%' as any,
+  },
+
+  headerButtonText: {
+    backgroundColor: '#131312',
+    color: '#FFFFFF',
+    fontFamily: 'arial',
+    fontSize: '150%' as any,
+    fontWeight: 500,
+  },
+
+  bodyButton: {
+    alignItems: 'center',
+    backgroundColor: '#131312',
+    borderRadius: 50,
+    borderWidth: 4,
+    borderColor: '#131312',
+    marginBottom: '2.5%',
+    padding: '2.5%' as any,
     width: '80%' as any,
-    height: '12.5%' as any
   },
 
-  buttonDisabled: {
-    backgroundColor: '#CCCCCC',
+  bodyButtonText: {
+    backgroundColor: 'transparent',
+    color: '#FFFFFF',
+    fontFamily: 'arial',
+    fontSize: '175%' as any,
+    fontWeight: 600,
+    textAlign: 'center',
   },
 
-  buttonText: {
-    color: 'white',
+  headerTitle: {
+    backgroundColor: '#C5151A',
+    color: '#FFFFFF',
+    fontFamily: 'arial',
+    fontSize: '300%' as any,
+    fontWeight: 800,
+    textAlign: 'center',
+    textShadowColor: '#131312',
+    textShadowOffset: { width: 2, height: 2 },
+    textShadowRadius: 10,
+    width: '50%' as any,
+  },
 
-    fontFamily: 'sans-serif',
-    fontSize: '200%' as any,
-    fontWeight: '800',
-  }
+  headerUsernameIcon: {
+    backgroundColor: '#C5151A',
+    color: '#FFFFFF',
+    fontFamily: 'arial',
+    fontSize: '150%' as any,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    width: '10%' as any,
+  },
+
+  infoText: {
+    backgroundColor: '#C5151A',
+    color: '#FFFFFF',
+    fontFamily: 'arial',
+    fontSize: '150%' as any,
+    fontWeight: 500,
+    marginTop: '0.5%' as any,
+    paddingTop: '5.0%' as any,
+    paddingBottom: '5.0%' as any,
+    textAlign: 'center',
+    textShadowColor: '#131312',
+    textShadowOffset: { width: 2, height: 2 },
+    textShadowRadius: 10,
+    width: '100%' as any,
+  },
 
 });
