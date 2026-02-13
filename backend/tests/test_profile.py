64122b63712452bdae42caf81b5895d8985747e4
Indecisive613 <fvcheng613@gmail.com>
2026-02-13 14:14:12 -0500

    feature: Add user deletion tests

--- backend/tests/test_profile.py
+++ backend/tests/test_profile.py
@@ -1,5 +1,9 @@
 # Project imports
-from backend.tests.test_helpers import seeded_users, add_test_user
+from backend.tests.test_helpers import (
+    seeded_users,
+    add_test_user,
+    seeded_transactions,
+)
 
 # Arguments like client and app are automatically injected from conftest.py
 # seeded_food_data is automatically injected from test_helpers.py
@@ -250,3 +254,82 @@ def test_edit_user_profile_no_optional_args(client, seeded_users):
 
     assert response.status_code == 200
     assert data["has_configured_settings"] is True
+
+
+# -------------------------------------
+# Testing Deleting User and their Data
+# -------------------------------------
+
+
+def test_delete_user_invalid_username(client, seeded_users):
+    response = client.delete("/profile/Alison", json={"password": "Password123!"})
+    data = response.get_json()
+
+    assert response.status_code == 400
+    assert "No matching username" in data["message"]
+
+
+def test_delete_user_invalid_password(client, seeded_users):
+    # Try missing password field
+    response = client.delete("/profile/Alice", json={"hello": "Alice"})
+    data = response.get_json()
+
+    assert response.status_code == 400
+    assert "password is required" in data["message"]
+
+    # Try wrong password
+    response = client.delete("/profile/Alice", json={"password": "AliceIsMe123!"})
+    data = response.get_json()
+
+    assert response.status_code == 400
+    assert "Password does not match" in data["message"]
+
+    # Check that the user has Alice has not been deleted (can't reuse username)
+    response = client.post(
+        "/auth/register", json={"username": "Alice", "password": "aaaaaBBBBB1@!"}
+    )
+
+    data = response.get_json()
+
+    assert response.status_code == 409
+    assert data["status"] == "error"
+    assert "This username is already in use." in data["message"]
+
+
+def test_delete_user_valid(client, seeded_users, seeded_transactions):
+    # Set Alice to be the only user contributing to global stats
+    client.post(
+        "/profile/edit",
+        json={"show_stats": True, "username": "Alice"},
+    )
+
+    response = client.get("/statistics/global?days=5")
+    data = response.get_json()
+
+    # Ensure trending items are influenced solely by Alice
+    assert data["trending_item_1"] == "Hamburger"
+    assert data["trending_item_2"] == "Caesar Salad"
+
+    # Now delete Alice and her data
+    response = client.delete("/profile/Alice", json={"password": "Password123!"})
+    data = response.get_json()
+
+    assert response.status_code == 200
+    assert "User deleted successfully" in data["message"]
+
+    # Check that Alice's logs have been deleted (trending items lost Alice's logs)
+    response = client.get("/statistics/global?days=5")
+    data = response.get_json()
+    assert data["trending_item_1"] == "Unknown"
+    assert data["trending_item_2"] == "Unknown"
+
+    # Check that the username Alice is now free
+    response = client.post(
+        "/auth/register", json={"username": "Alice", "password": "aaaaaBBBBB1@!"}
+    )
+
+    data = response.get_json()
+
+    assert response.status_code == 201
+    assert data["status"] == "success"
+    assert "User registered successfully." in data["message"]
