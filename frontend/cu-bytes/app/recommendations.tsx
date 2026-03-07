8528f8d51cfb048be798ec7a3f8b14a1b05c07d9
Jake Siushansian <jakesiushansian@cmail.carleton.ca>
2026-03-06 21:58:22 -0500

    feat: connected the recommendations backend endpoints to the recommendations frontend page

--- frontend/cu-bytes/app/recommendations.tsx
+++ frontend/cu-bytes/app/recommendations.tsx
@@ -8,7 +8,366 @@ import { styles } from './_styles/style-recommendations';
 import { useUser } from './_context';
 import { API_BASE_URL } from '@/services/api';
 
-export default function Recommendations() {
+export default function RecommendationsScreen() {
 
-    
+    const [loading, setLoading] = useState(false);
+
+    /*
+        Variable and setter used to track whether one of the recommendation mode buttons has been pressed
+        Initialized with the value false
+        Set to the value true everytime a recommendation mode button is pressed
+        Set to the value false everytime a recommendation fetch button is pressed
+    */
+    const [recommendationModeButtonPressed, setRecommendationModeButtonPressed] = useState(false);
+
+    /*
+        Variable and setter used to track what recommendation mode button has specifically been pressed
+        Initialized with the value ""
+        Set to the value "Trending", "Random", "Ideal", "Nutrient", or "Similar" everytime a recommendation mode button is pressed
+        Set to the value "" everytime a recommendation fetch button is pressed
+    */
+    const [selectedRecommendationMode, setSelectedRecommendationMode] = useState("");
+
+    /*
+        Variable and setter used to track whether any recommendations have been fetched from the backend
+        Initialized with the value false
+        Set to the value true everytime a recommendation fetch button is pressed
+    */
+    const [fetchedRecommendations, setFetchedRecommendations] = useState(false);
+
+    const [isBackPressed, setIsBackPressed] = useState(false);
+    const [isLoginLogoutPressed, setIsLoginLogoutPressed] = useState(false);
+    const [isTrendingPressed, setIsTrendingPressed] = useState(false);
+    const [isRandomPressed, setIsRandomPressed] = useState(false);
+    const [isIdealPressed, setIsIdealPressed] = useState(false);
+    const [isNutrientPressed, setIsNutrientPressed] = useState(false);
+    const [isSimilarPressed, setIsSimilarPressed] = useState(false);
+
+    const [numberOfFoodItems, setNumberOfFoodItems] = useState(3);
+    const [numberOfUsers, setNumberOfUsers] = useState(10);
+
+    /*
+        Variables and setters used to store a copy of the logged-in user's username and profile settings
+        (Frontend copy updated based on the backend data)
+    */
+    const {
+        usernameGlobal,
+        hasEggAllergyGlobal,
+        hasFishOrShellfishAllergyGlobal,
+        hasDairyIntoleranceGlobal,
+        hasMilkAllergyGlobal,
+        hasPeanutAllergyGlobal,
+        hasSesameAllergyGlobal,
+        hasSoyAllergyGlobal,
+        hasTreenutAllergyGlobal,
+        hasWheatAllergyGlobal,
+        hasGlutenAllergyGlobal,
+        isVeganGlobal,
+        isVegetarianGlobal,
+        prefersHalalGlobal,
+        setUsernameGlobal,
+        setShowStatsGlobal,
+        setHasEggAllergyGlobal,
+        setHasFishOrShellfishAllergyGlobal,
+        setHasDairyIntoleranceGlobal,
+        setHasMilkAllergyGlobal,
+        setHasPeanutAllergyGlobal,
+        setHasSesameAllergyGlobal,
+        setHasSoyAllergyGlobal,
+        setHasTreenutAllergyGlobal,
+        setHasWheatAllergyGlobal,
+        setHasGlutenAllergyGlobal,
+        setIsVeganGlobal,
+        setIsVegetarianGlobal,
+        setPrefersHalalGlobal
+
+    } = useUser();
+
+    /*
+        Variable and setter for storing and modifying the trending recommendations of the logged-in user
+    */
+    const [trendingRecommendations, setTrendingRecommendations] = useState(
+        {
+            "food_items": [
+                {
+                "dining_location": "Unknown",
+                "id": -1,
+                "name": "Unknown"
+                },
+                {
+                "dining_location": "Unknown",
+                "id": -1,
+                "name": "Unknown"
+                },
+                {
+                "dining_location": "Unknown",
+                "id": -1,
+                "name": "Unknown"
+                }
+            ]
+        }
+    )
+
+    /*
+        Variable and setter for storing and modifying the random recommendations of the logged-in user
+    */
+    const [randomRecommendations, setRandomRecommendations] = useState(
+        {
+            "food_items": [
+                {
+                "dining_location": "Unknown",
+                "id": -1,
+                "name": "Unknown"
+                },
+                {
+                "dining_location": "Unknown",
+                "id": -1,
+                "name": "Unknown"
+                },
+                {
+                "dining_location": "Unknown",
+                "id": -1,
+                "name": "Unknown"
+                }
+            ]
+        }
+    )
+
+    /*
+        Variable and setter for storing and modifying the ideal recommendations of the logged-in user
+    */
+    const [idealRecommendations, setIdealRecommendations] = useState(
+        {
+            "food_items": [
+                {
+                "dining_location": "Unknown",
+                "id": -1,
+                "name": "Unknown"
+                },
+                {
+                "dining_location": "Unknown",
+                "id": -1,
+                "name": "Unknown"
+                },
+                {
+                "dining_location": "Unknown",
+                "id": -1,
+                "name": "Unknown"
+                }
+            ]
+        }
+    )
+
+    /*
+        Variable and setter for storing and modifying the nutrient recommendations of the logged-in user
+    */
+    const [nutrientRecommendations, setNutrientRecommendations] = useState(
+        {
+            "food_items": [
+                {
+                "dining_location": "Unknown",
+                "id": -1,
+                "name": "Unknown"
+                },
+                {
+                "dining_location": "Unknown",
+                "id": -1,
+                "name": "Unknown"
+                },
+                {
+                "dining_location": "Unknown",
+                "id": -1,
+                "name": "Unknown"
+                }
+            ]
+        }
+    )
+
+    /*
+        Variable and setter for storing and modifying the similar recommendations of the logged-in user
+    */
+    const [similarRecommendations, setSimilarRecommendations] = useState(
+        {
+            "deficient_nutrient": "Unknown",
+            "food_items": [
+                {
+                "dining_location": "Unknown",
+                "id": -1,
+                "name": "Unknown"
+                },
+                {
+                "dining_location": "Unknown",
+                "id": -1,
+                "name": "Unknown"
+                },
+                {
+                "dining_location": "Unknown",
+                "id": -1,
+                "name": "Unknown"
+                }
+            ]
+        }
+    )
+
+    /*
+        Log out the logged-in user by setting their username and profile settings to null 
+    */
+    const logout = () => {
+
+        setUsernameGlobal('');
+        setShowStatsGlobal(false);
+        setHasEggAllergyGlobal(false);
+        setHasFishOrShellfishAllergyGlobal(false);
+        setHasDairyIntoleranceGlobal(false);
+        setHasMilkAllergyGlobal(false);
+        setHasPeanutAllergyGlobal(false);
+        setHasSesameAllergyGlobal(false);
+        setHasSoyAllergyGlobal(false);
+        setHasTreenutAllergyGlobal(false);
+        setHasWheatAllergyGlobal(false);
+        setHasGlutenAllergyGlobal(false);
+        setIsVeganGlobal(false);
+        setIsVegetarianGlobal(false);
+        setPrefersHalalGlobal(false);
+
+        router.push('/');
+    }
+
+    /*
+        Send a request to the backend endpoint to get the logged-in user's trending recommendations
+
+        param(s):
+            items - number : The number of fooditems to return
+    */
+    const getTrendingRecs = async (items: number) => {
+        try {
+            fetch(`${API_BASE_URL}/recommend/trending/${usernameGlobal}?items=${items}`)
+                .then( response => response.json() )
+                .then( data => {
+                    setTrendingRecommendations(data);
+                    console.log(trendingRecommendations);
+                })
+                .catch( error => { console.log(error) });
+        } catch (err) {
+            console.log(err);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+   /*
+        Send a request to the backend endpoint to get the logged-in user's random recommendations
+
+        param(s):
+            items - number : The number of fooditems to return
+   */
+    const getRandomRecs = async (items: number) => {
+        try {
+            fetch(`${API_BASE_URL}/recommend/random/${usernameGlobal}?items=${items}`)
+                .then( response => response.json() )
+                .then( data => {
+                    setRandomRecommendations(data);
+                    console.log(randomRecommendations);
+                })
+                .catch( error => { console.log(error) });
+        } catch (err) {
+            console.log(err);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    /*
+        Send a request to the backend endpoint to get the logged-in user's ideal recommendations
+
+        param(s):
+            items - number : The number of fooditems to return
+    */
+    const getIdealRecs = async (items: number) => {
+        try {
+            fetch(`${API_BASE_URL}/recommend/ideal/${usernameGlobal}?items=${items}`)
+                .then( response => response.json() )
+                .then( data => {
+                    setIdealRecommendations(data);
+                    console.log(idealRecommendations);
+                })
+                .catch( error => { console.log(error) });
+        } catch (err) {
+            console.log(err);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    /*
+        Send a request to the backend endpoint to get the logged-in user's nutrient recommendations
+
+        param(s):
+            items - number : The number of fooditems to return
+    */
+    const getNutrientRecs = async (items: number) => {
+        try {
+            fetch(`${API_BASE_URL}/recommend/nutrient/${usernameGlobal}?items=${items}`)
+                .then( response => response.json() )
+                .then( data => {
+                    setNutrientRecommendations(data);
+                    console.log(nutrientRecommendations);
+                })
+                .catch( error => { console.log(error) });
+        } catch (err) {
+            console.log(err);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    /*
+        Send a request to the backend endpoint to get the logged-in user's similar recommendations
+
+        param(s):
+            items - number : The number of fooditems to return
+            users - number : The number of users to compare with
+    */
+    const getSimilarRecs = async (items: number, users: number) => {
+        try {
+            fetch(`${API_BASE_URL}/recommend/similar/${usernameGlobal}?items=${items}&users=${users}`)
+                .then( response => response.json() )
+                .then( data => {
+                    setSimilarRecommendations(data);
+                    console.log(similarRecommendations);
+                })
+                .catch( error => { console.log(error) });
+        } catch (err) {
+            console.log(err);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    /*
+        Display the loading symbol while food items are being retrieved or logged
+    */
+    if (loading) {
+        return (
+            <View>
+                <ActivityIndicator size="large" />
+            </View>
+        );
+    }
+
+    return (
+
+        <View style={styles.container}>
+            <StatusBar
+                style="auto"
+                hidden={true}
+            />
+
+            <View style={styles.statusbar}>
+
+            </View>
+
+        </View>
+    )
+     
 }
